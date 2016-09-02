164ef85ed28fba10c6653ee092bf91b09b2037a0
rhadiyazgamaputra32 <radiasgama@gmail.com>
2016-09-02 11:30:07 +0700

    praktikum2_5
    praktikum2_5

---
+++ _2.5[1].ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+DDRC = 0x00;
+PORTC = 0x01;
+DDRB = 0x10;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if (bit_is_clear(PINC,0)){
+  PORTB = 0x10;
+}
+else {
+  PORTB = 0x00;
+}
+}
